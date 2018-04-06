cfc04c143905cb7fcb591105a67a107e7d11ddcd
James Taggart <jxt1607@g.rit.edu>
2018-04-05 20:58:34 -0400

    Created menu framework, build order

---
+++ Assets/Scripts/UI/TransferScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TransferScene : MonoBehaviour {
+
+    public enum ToScene { MainMenu,Game,Credits,Options};
+    public ToScene sceneTarget;
+    private string targetString;
+
+
+	// Use this for initialization
+	void Start () {
+        switch (sceneTarget)
+        {
+            case ToScene.MainMenu:
+                {
+                    targetString = "MainMenu";
+                    break;
+                }
+            case ToScene.Game:
+                {
+                    targetString = "Game";
+                    break;
+                }
+            case ToScene.Credits:
+                {
+                    targetString = "Credits";
+                    break;
+                }
+            case ToScene.Options:
+                {
+                    targetString = "Options";
+                    break;
+                }
+
+        }
+	}
+	
+	public void OnClick()
+    {
+        SceneManager.LoadScene(targetString);
+    }
+}

83c66e956910d08a7910f61924d9ccb776cf2ed8
James Taggart <jxt1607@g.rit.edu>
2018-04-23 11:05:30 -0400

    Audio Stuff

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AudioSource chopSource;
+    public AudioSource beeSource;
+    public AudioSource ouchSource;
+    public AudioSource musicSource;
+
+    public AudioClip chopClip;
+    public AudioClip beeClip;
+    public AudioClip ouchClip;
+    public AudioClip musicClip;
+
+    public static SoundManager instance = null;
+
+    public float volume = 0.5f;
+
+    public bool muted = false;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayOuch()
+    {
+        if(!muted)
+            ouchSource.PlayOneShot(ouchClip, volume);
+    }
+
+    public void PlayChop()
+    {
+        if (!muted)
+            chopSource.PlayOneShot(chopClip, volume);
+    }
+
+    public void PlayBees()
+    {
+        if (!muted)
+            beeSource.PlayOneShot(beeClip, volume);
+    }
+
+    public void MuteAudio()
+    {
+        if (muted)
+            muted = false;
+        else
+            muted = true;
+    }
+
+
 }
